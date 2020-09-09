75ed97fda9fe9998392ab9cb0bf627a74905e4b4
Farshid <farshidnooshi726@aut.ac.ir>
2020-09-09 22:06:23 +0430

    cors

--- Back end/Polaris/API/Startup.cs
+++ Back end/Polaris/API/Startup.cs
@@ -25,12 +25,16 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddScoped<,>(); // TODO
             services.AddControllers();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(options =>  // can be checked
+                        options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
