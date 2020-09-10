30d79056c2119e1f5fa47838ecbb2c7da65769a3
Saba <saba.eb18@gmail.com>
2020-09-10 17:36:55 +0430

    search bar complete without color

--- Front end/Polaris/src/app/app.module.ts
+++ Front end/Polaris/src/app/app.module.ts
@@ -3,22 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { GraphScreenComponent } from './graph-screen/graph-screen.component';
-import { InfoSideBarComponent } from './info-side-bar/info-side-bar.component';
 import { LeftPanelComponent } from './left-panel/left-panel.component';
 import { RightPanelComponent } from './right-panel/right-panel.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
     AppComponent,
     GraphScreenComponent,
-    InfoSideBarComponent,
     LeftPanelComponent,
     RightPanelComponent,
     SearchBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
