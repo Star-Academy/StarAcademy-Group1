a205c7fcdb79dbb0b23629698a46a7884e82c3d6
mahla <aa@ya.com>
2020-09-11 11:14:47 -0600

    Add new service

---
+++ Front end/Polaris/src/services/nav-side-open-close.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import * as Ogma from '../assets/ogma.min.js';
+@Injectable()
+export class RandomGraphService{
+    public ogma:Ogma;
+
+    public initConfig(configuration = {}) {
+        this.ogma = new Ogma(configuration);
+    }
+
+    public runLayout(): Promise<void> {
+        return this.ogma.layouts.force({ locate: true });
+    }
+
+    public getJsonGraph() {
+        this.ogma.parse.jsonFromUrl('../assets/data2.json').then((graph) => this.setGraph(graph));
+    }
+
+    public setGraph(graph) {
+        this.ogma.setGraph(graph);
+        this.runLayout();
+    }
+}
