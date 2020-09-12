c71856b46183684767b874326c6528bf79145a54
Farshid <farshidnooshi726@aut.ac.ir>
2020-09-12 15:54:32 +0430

    bfs is waiting for the meeting

--- Back end/Polaris/Analysis/Analyser/Bfs.cs
+++ Back end/Polaris/Analysis/Analyser/Bfs.cs
@@ -4,10 +4,11 @@ using Analysis.GraphStructure.Structures;
 using Elastic.Models;
 using Elasticsearch.Net;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Analysis.Analyser
 {
-    class BFS<NID, NDATA, EID, EDATA> 
+    class BFS<NID, NDATA, EID, EDATA>
         where NDATA : Entity<NID>
         where EDATA : Entity<EID>
     {
@@ -32,7 +33,40 @@ namespace Analysis.Analyser
             queue[0] = new List<NID>();
             queue[1] = new List<NID>();
 
+            queue[0].Add(source.Id);
+            visited[0][source.Id] = true;
 
+            queue[1].Add(target.Id);
+            visited[1][target.Id] = true;
+
+            while (!queue[0].Any() && !queue[1].Any())
+            {
+                BreadthFirstSearch(ref queue[0], ref visited[0], ref parent[0]);
+                BreadthFirstSearch(ref queue[1], ref visited[1], ref parent[1]);
+
+                // should check for a good path :)
+            }
+        }
+
+        private void BreadthFirstSearch(ref List<NID> queue, ref Dictionary<NID, bool> visited, ref Dictionary<NID, NID> parent)
+        {
+            while (!queue.Any())
+            {
+                NID current = queue.First();
+                queue.RemoveAt(0);
+                if (visited[current])
+                    continue;
+                var list = graph.GetNeighbors(current);
+                foreach (var node in list)
+                {
+                    if (!visited[node.Id])
+                    {
+                        parent[node.Id] = current;
+                        visited[node.Id] = true;
+                        queue.Add(node.Id);
+                    }
+                }
+            }
         }
     }
 }
