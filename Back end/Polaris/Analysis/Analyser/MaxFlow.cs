489c51a66c423f405d120189d00e851ddf0b3423
Farshid <farshidnooshi726@aut.ac.ir>
2020-09-13 14:20:32 +0430

    max flow start

---
+++ Back end/Polaris/Analysis/Analyser/MaxFlow.cs
@@ -0,0 +1,16 @@
+﻿using Analysis.GraphStructure;
+using Elastic.Models;
+
+namespace Analysis.Analyser
+{
+    class MaxFlow<NID, NDATA, EID, EDATA>
+        where NDATA : Entity<NID>
+        where EDATA : Entity<EID>
+    {
+        private readonly Graph<NID, NDATA, EID, EDATA> graph;
+        public MaxFlow(Graph<NID, NDATA, EID, EDATA> graph)
+        {
+            this.graph = graph;
+        }
+    }
+}
