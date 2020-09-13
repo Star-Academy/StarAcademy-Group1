e6c30aa39d5c93110543081d1b7502d691b3aad0
Farshid <farshidnooshi726@aut.ac.ir>
2020-09-13 10:55:40 +0430

    refactor

--- Back end/Polaris/Analysis/Analyser/Bfs.cs
+++ Back end/Polaris/Analysis/Analyser/Bfs.cs
@@ -22,84 +22,67 @@ namespace Analysis.Analyser
 
         public void BiDirectionalSearch(Node<NID, NDATA> source, Node<NID, NDATA> target, Filter filter)
         {
-            var nodes = new HashSet<NID>();
-            
-            Dictionary<NID, bool>[] visited = new Dictionary<NID, bool>[2];
-            visited[0] = new Dictionary<NID, bool>();
-            visited[1] = new Dictionary<NID, bool>();
+            var edges = new HashSet<Edge<EID, EDATA, Node<NID, NDATA>>>();
 
-            Dictionary<NID, NID>[] parent = new Dictionary<NID, NID>[2];
-            parent[0] = new Dictionary<NID, NID>();
-            parent[1] = new Dictionary<NID, NID>();
+            Dictionary<NID, List<LinkedList<NID>>>[] paths = new Dictionary<NID, List<LinkedList<NID>>>[2];
+            paths[0] = new Dictionary<NID, List<LinkedList<NID>>>();
+            paths[1] = new Dictionary<NID, List<LinkedList<NID>>>();
 
-            List<NID>[] queue = new List<NID>[2];
-            queue[0] = new List<NID>();
-            queue[1] = new List<NID>();
+            List<LinkedList<NID>>[] queue = new List<LinkedList<NID>>[2];
+            queue[0] = new List<LinkedList<NID>>();
+            queue[1] = new List<LinkedList<NID>>();
 
-            queue[0].Add(source.Id);
-            visited[0][source.Id] = true;
-            parent[0][source.Id] = source.Id;
+            LinkedList<NID> path = new LinkedList<NID>();
+            path.AddLast(source.Id);
 
-            queue[1].Add(target.Id);
-            visited[1][target.Id] = true;
-            parent[1][target.Id] = target.Id;
+            queue[0].Add(path);
+            paths[0][source.Id].Add(path);
+
+            path.Clear();
+            path.AddLast(target.Id);
+
+            queue[1].Add(path);
+            paths[1][target.Id].Add(path);
 
             while (!queue[0].Any() && !queue[1].Any())
             {
-                BreadthFirstSearch(ref queue[0], ref visited[0], ref parent[0]);
-                BreadthFirstSearch(ref queue[1], ref visited[1], ref parent[1]);
+                BreadthFirstSearch(ref queue[0], ref paths[0]);
+                BreadthFirstSearch(ref queue[1], ref paths[1], 0);
+            }
 
-                var commonNodes = Intersect(ref visited);
-                if (commonNodes.Any())
+            foreach (var item in graph.Adj)
+            {
+                if (paths[0][item.Key.Id].Any() && paths[1][item.Key.Id].Any())
                 {
-                    foreach (var item in commonNodes)
-                        Add(item, ref nodes, source.Id, target.Id, parent);
-                }
+                    TakePath(item.Key, ref edges);
+                } 
             }
         }
 
-        private void Add(NID id, ref HashSet<NID> set, NID source, NID target, Dictionary<NID, NID> [] parent)
+        private void TakePath(Node<NID, NDATA> node, ref HashSet<Edge<EID, EDATA, Node<NID, NDATA>>> edges)
         {
-            NID temp = id;
-            while (temp.GetHashCode() != source.GetHashCode())
-            {
-                set.Add(temp);
-                temp = parent[0][temp];
-            }
 
-            temp = id;
-            while (temp.GetHashCode() != target.GetHashCode())
-            {
-                set.Add(temp);
-                temp = parent[1][temp];
-            }
         }
 
-        private List<NID> Intersect(ref Dictionary<NID, bool>[] visited)
-        {
-            List<NID> ret = new List<NID>();
-            foreach (var item in visited[0])
-                if (visited[1].ContainsKey(item.Key))
-                    ret.Add(item.Key);
-            return ret;
-        }
-
-        private void BreadthFirstSearch(ref List<NID> queue, ref Dictionary<NID, bool> visited, ref Dictionary<NID, NID> parent)
+        private void BreadthFirstSearch(ref List<LinkedList<NID>> queue, ref Dictionary<NID, List<LinkedList<NID>>> paths, int src = 1)
         {
             while (!queue.Any())
             {
-                NID current = queue.First();
+                var current = queue.First();
                 queue.RemoveAt(0);
-                if (visited[current])
-                    continue;
-                var list = graph.GetNeighbors(current);
+                var last = current.Last();
+                var list = graph.GetNeighbors(last);
+                if (src == 0)
+                    list = graph.GetOpositeNeighbors(last);
                 foreach (var node in list)
                 {
-                    if (!visited[node.Id])
+                    if (!current.Contains(node.Id))
                     {
-                        parent[node.Id] = current;
-                        visited[node.Id] = true;
-                        queue.Add(node.Id);
+                        LinkedList<NID> newPath = current;
+                        newPath.AddLast(node.Id);
+                        if (newPath.Count() > 3 + src) continue;
+                        paths[node.Id].Add(newPath);
+                        queue.Add(newPath);
                     }
                 }
             }
--- Back end/Polaris/Analysis/GraphStructure/Graph.cs
+++ Back end/Polaris/Analysis/GraphStructure/Graph.cs
@@ -1,6 +1,7 @@
 // In The Name Of GOD
 using Analysis.GraphStructure.Structures;
 using Elastic.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -11,6 +12,8 @@ namespace Analysis.GraphStructure
     where EDATA : Entity<EID>
     {
         public Dictionary<Node<NID, NDATA>, LinkedList<Edge<EID, EDATA, Node<NID, NDATA>>>> Adj { get; set; }
+        private readonly Dictionary<Node<NID, NDATA>, LinkedList<Edge<EID, EDATA, Node<NID, NDATA>>>> reverseAdj;
+
         public Dictionary<NID, Node<NID, NDATA>> IDToNode { get; set; }
         public Dictionary<NDATA, Node<NID, NDATA>> DataToNode { get; set; }
         public List<Node<NID, NDATA>> GetNeighbors(NDATA data)
@@ -25,6 +28,8 @@ namespace Analysis.GraphStructure
             {
                 IDToNode[item.Key.Id] = item.Key;
                 DataToNode[item.Key.Data] = item.Key;
+                foreach (var edge in item.Value)
+                    reverseAdj[edge.Target].AddLast(edge);
             }
         }
 
@@ -43,6 +48,19 @@ namespace Analysis.GraphStructure
         {
             var node = IDToNode[id];
             return ReadNeighbors(node);
+        }
+
+        public List<Node<NID, NDATA>> GetOpositeNeighbors(NID id)
+        {
+            var node = IDToNode[id];
+            
+            var set = new HashSet<Node<NID, NDATA>>();
+            foreach (var edge in reverseAdj[node])
+            {
+                set.Add(edge.Source);
+            }
+
+            return set.ToList();
         }
     }
 }
