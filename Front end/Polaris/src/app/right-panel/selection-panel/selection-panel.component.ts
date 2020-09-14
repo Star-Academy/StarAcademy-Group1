dbac18aec9a4040331977ff9ea576def6c16f604
Saba <saba.eb18@gmail.com>
2020-09-14 12:03:25 +0430

    selection panel without search

--- Front end/Polaris/src/app/right-panel/selection-panel/selection-panel.component.ts
+++ Front end/Polaris/src/app/right-panel/selection-panel/selection-panel.component.ts
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
-import { Component, OnInit, OnChanges, SimpleChanges } from '@angular/core';
-import { ComponentsCommunication } from 'src/services/components-communication.service';
-import { RandomGraphService } from './../../../services/read-ogma-from-random-json.service';
-=======
 import { Component, OnInit } from '@angular/core';
 import { ComponentsCommunicationService } from 'src/services/components-communication.service';
->>>>>>> 82c391c34bee8ee3a39a8c206ea818abbd5cab2d
-
+import { OgmaHandlerService } from './../../../services/ogma-handler.service';
 
 @Component({
   selector: 'app-selection-panel',
@@ -15,7 +9,7 @@ import { ComponentsCommunicationService } from 'src/services/components-communic
 })
 export class SelectionPanelComponent implements OnInit {
   public hidden = false;
-  constructor(public componentCommunication: ComponentsCommunication, public randomOgma: RandomGraphService) { }
+  constructor(public componentCommunication: ComponentsCommunicationService, public randomOgma: OgmaHandlerService) { }
 
   ngOnInit(): void {
 
@@ -27,6 +21,7 @@ export class SelectionPanelComponent implements OnInit {
   public closeNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
+
   public getNodeById(nodeId: string): string {
     return this.randomOgma.ogma.getNode(nodeId).getData('OwnerName') + " " + this.randomOgma.ogma.getNode(nodeId).getData('OwnerFamilyName');
   }
