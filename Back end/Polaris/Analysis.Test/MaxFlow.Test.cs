becbd057bb266b0e371fae53586e90401500a22a
Farshid <farshidnooshi726@aut.ac.ir>
2020-09-15 10:28:02 +0430

    max flow tests passed

--- Back end/Polaris/Analysis.Test/MaxFlow.Test.cs
+++ Back end/Polaris/Analysis.Test/MaxFlow.Test.cs
@@ -22,17 +22,6 @@ namespace Analysis.Test
                 node.Data = new Data(i);
                 dic[node] = new List<Edge<int, Data, Node<int, Data>>>();
             }
-            AddEdge(0, 1, 16);
-            AddEdge(0, 2, 13);
-            AddEdge(1, 2, 10);
-            AddEdge(1, 3, 12);
-            AddEdge(2, 1, 4);
-            AddEdge(2, 4, 14);
-            AddEdge(3, 2, 9);
-            AddEdge(3, 5, 20);
-            AddEdge(4, 3, 7);
-            AddEdge(4, 5, 4);
-            graph = new Graph<int, Data, int, Data>(dic);
         }
 
         private void AddEdge(int src, int tar, Int64 amount)
@@ -57,8 +46,56 @@ namespace Analysis.Test
         public void Test1()
         {
             Init();
+            AddEdge(0, 1, 16);
+            AddEdge(0, 2, 13);
+            AddEdge(1, 2, 10);
+            AddEdge(1, 3, 12);
+            AddEdge(2, 1, 4);
+            AddEdge(2, 4, 14);
+            AddEdge(3, 2, 9);
+            AddEdge(3, 5, 20);
+            AddEdge(4, 3, 7);
+            AddEdge(4, 5, 4);
+            graph = new Graph<int, Data, int, Data>(dic);
             var flow = new MaxFlow<int, Data, int, Data>(graph);
             Assert.AreEqual(23, flow.DinicMaxFlow(0, 5));
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            Init();
+            AddEdge(0, 1, 3);
+            AddEdge(0, 2, 7);
+            AddEdge(1, 3, 9);
+            AddEdge(1, 4, 9);
+            AddEdge(2, 1, 9);
+            AddEdge(2, 4, 9);
+            AddEdge(2, 5, 4);
+            AddEdge(3, 5, 3);
+            AddEdge(4, 5, 7);
+            AddEdge(0, 4, 10);
+            graph = new Graph<int, Data, int, Data>(dic);
+            var flow = new MaxFlow<int, Data, int, Data>(graph);
+            Assert.AreEqual(14, flow.DinicMaxFlow(0, 5));
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            Init();
+            AddEdge(0, 1, 10);
+            AddEdge(0, 2, 10);
+            AddEdge(1, 3, 4);
+            AddEdge(1, 4, 8);
+            AddEdge(1, 2, 2);
+            AddEdge(2, 4, 9);
+            AddEdge(3, 5, 10);
+            AddEdge(4, 3, 6);
+            AddEdge(4, 5, 10);
+            graph = new Graph<int, Data, int, Data>(dic);
+            var flow = new MaxFlow<int, Data, int, Data>(graph);
+            Assert.AreEqual(19, flow.DinicMaxFlow(0, 5));
         }
     }
 }
