0e548b658096ee5869be5b3900f4b8219568e95d
mahla <aa@ya.com>
2020-09-16 05:36:11 -0600

    init nodes veiw panel

--- Front end/Polaris/src/app/right-panel/view-panel/nodes-view-panel/nodes-view-panel.component.ts
+++ Front end/Polaris/src/app/right-panel/view-panel/nodes-view-panel/nodes-view-panel.component.ts
@@ -1,15 +1,33 @@
+
 import { Component, OnInit } from '@angular/core';
+import { ComponentsCommunicationService } from 'src/services/components-communication.service';
+import { OgmaHandlerService } from './../../../../services/ogma-handler.service';
 
 @Component({
-  selector: 'app-nodes-view-panel',
+  selector: 'nodes-view-panel-panel',
   templateUrl: './nodes-view-panel.component.html',
   styleUrls: ['./nodes-view-panel.component.scss']
 })
 export class NodesViewPanelComponent implements OnInit {
-
-  constructor() { }
+  public hidden = false;
+  constructor(public componentCommunication: ComponentsCommunicationService, public randomOgma: OgmaHandlerService) { }
 
   ngOnInit(): void {
+
+  }
+  public openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
   }
 
+  public closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+
+  public getNodeById(nodeId: string): string {
+    return this.randomOgma.ogma.getNode(nodeId).getData('OwnerName') + " " + this.randomOgma.ogma.getNode(nodeId).getData('OwnerFamilyName');
+  }
+
+  public isSelectedNodeById(nodeId: string): boolean {
+    return this.randomOgma.ogma.getNode(nodeId).isSelected();
+  }
 }
