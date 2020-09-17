2ba1b268f0d2206bff8301171eb7fcd8b951700c
Parsa <parsa2820@gmail.com>
2020-09-17 16:25:12 +0430

    Implement TextNestCriteria operators

--- Back end/Polaris/Elastic/Filtering/Criteria/TextNestCriteria.cs
+++ Back end/Polaris/Elastic/Filtering/Criteria/TextNestCriteria.cs
@@ -1,16 +1,61 @@
 using Nest;
+using System;
+using System.Collections.Generic;
+using Elastic.Filtering.Attributes;
 
 namespace Elastic.Filtering.Criteria
 {
+    using OperatorToFunctionDict = Dictionary<string, Func<TextNestCriteria, string, string, QueryContainer>>;
     public class TextNestCriteria : NestCriteria
     {
+        private static OperatorToFunctionDict registry = GetRegistry<TextNestCriteria>();
+
         public TextNestCriteria(string field, string @operator, string value) : base(field, @operator, value)
         {
         }
 
+        [NestOperator("eq")]
+        public static QueryContainer Equal(string field, string value)
+        {
+            return new MatchQuery
+            {
+                Field = field,
+                Query = value
+            };
+        }
+
+        [NestOperator("nq")]
+        public static QueryContainer NotEqual(string field, string value)
+        {
+            return new BoolQuery
+            {
+                MustNot = new List<QueryContainer> { Equal(field, value) }
+            };
+        }
+
+        [NestOperator("sw")]
+        public static QueryContainer StartsWith(string field, string value)
+        {
+            return new PrefixQuery {
+                Field = field,
+                Value = value
+            };
+        }
+
+        [NestOperator("ew")]
+        public static QueryContainer EndsWith(string field, string value)
+        {
+            return new RegexpQuery {
+                Field = field,
+                Value = "*." + value
+            };
+        }
+
         public override QueryContainer Interpret()
         {
-            throw new System.NotImplementedException();
+            foreach (var item in registry)
+                Console.WriteLine(item.Key, item.Value);
+            return registry[Operator].Invoke(null, Field, Value);
         }
     }
 }
