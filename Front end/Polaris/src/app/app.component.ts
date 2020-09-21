8c0bc2d11809cb5bb9eae2896db719f3b8843741
mohammadhheidarzade <dr.mhheidarzade@gmail.com>
2020-09-21 22:04:14 +0330

    logo added

--- Front end/Polaris/src/app/app.component.ts
+++ Front end/Polaris/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { OgmaHandlerService } from 'src/services/ogma-handler.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Polaris';
+  public constructor(public ogmaService:OgmaHandlerService){
+  }
+
 }
