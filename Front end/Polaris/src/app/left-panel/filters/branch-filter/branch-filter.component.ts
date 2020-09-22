b7afd994d8cc2a2cc8bd5e683510aa3942f3d741
mahla <aa@ya.com>
2020-09-22 08:54:20 -0600

    try to fix real-time-search of branch filter

--- Front end/Polaris/src/app/left-panel/filters/branch-filter/branch-filter.component.ts
+++ Front end/Polaris/src/app/left-panel/filters/branch-filter/branch-filter.component.ts
@@ -8,22 +8,33 @@ import { FilterService } from 'src/services/filter.service';
   styleUrls: ['./branch-filter.component.scss'],
 })
 export class BranchFilterComponent implements OnInit {
-  @Input()
-  public branches: string[];
-
-  public selectedBranches: string[]=[];
-
+  // @Input()
+  public branches: string[] = ["آزادی - یادگار", "چهارراه وليعصر", "نازی آباد", "خیابان ایت اله طالقانی", "پاستور",
+  "گاندی", "میدان حسین آباد", "امیر آباد شمالی", "احمدیه", "استاد حسن بنا", "جمالزاده جنوبی",
+  "شهید صابونیان", "استاد نجات الهی شمالی", "چهارراه نیاکان", "سید خندان", "بیست متری نبرد"];
+  public selectedBranches: string[] = [];
+  public searchedBranches: string[] = [];
   constructor(
     public filterService: FilterService,
     public dataOnScreen: DataOnScreenService
   ) {}
 
   ngOnInit(): void {
-    this.branches = this.dataOnScreen.branchList;
+
   }
 
 
-  public updateResult(field: string){
+  public updateResult(input: string){
+    let index = this.selectedBranches.indexOf(branch, 0);
+    this.searchedBranches = this.branches;
+    this.searchedBranches.forEach((element) => {
+      console.log(element);
+      if (element.indexOf(input) != -1 )
+          this.searchedBranches.splice(element);
+      });
+  }
+  public isBranchSelected(branchName : string){
+    return this.selectedBranches.indexOf(branchName) === -1 ? false : true ;
   }
 
   public changeChecked(branch: string, isChecked: boolean) {
@@ -35,6 +46,8 @@ export class BranchFilterComponent implements OnInit {
     else if(!isChecked && index !=-1){
       this.selectedBranches.splice(index, 1);
     }
-    console.log(this.selectedBranches);
+  }
+  public h(){
+    console.log("helllo saba $ mahla");
   }
 }
