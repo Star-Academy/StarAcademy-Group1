69e76b19e76d5259234eb320aa66420fa2ce5591
Saba <saba.eb18@gmail.com>
2020-09-23 08:45:20 +0330

    change panel styles reset

--- Front end/Polaris/src/app/left-panel/panels-menu/panels-menu.component.ts
+++ Front end/Polaris/src/app/left-panel/panels-menu/panels-menu.component.ts
@@ -1,3 +1,5 @@
+import { ConstValuesService } from 'src/services/const-values.service';
+import { GraphHandlerService } from './../../services/main-graph.service';
 import { Component, OnInit } from '@angular/core';
 import { ComponentsCommunicationService } from 'src/services/components-communication.service';
 
@@ -12,7 +14,26 @@ export class PanelsMenuComponent implements OnInit {
   expandIconPath = '../../../assets/expandIcon.png';
   addNodeIconPath = '../../../assets/addIcon.png';
 
-  constructor(public componentCommunication: ComponentsCommunicationService) {}
+  constructor(public componentCommunication: ComponentsCommunicationService, private graphProvider: GraphHandlerService, private constVlues:ConstValuesService) {}
 
   ngOnInit(): void {}
+
+  public onIconClick(id: string){
+    if (id === 'selection-icon'){
+      this.componentCommunication.whichLeftSideNav = 1;
+    }
+    if (id === 'path-icon'){
+      this.componentCommunication.whichLeftSideNav = 2;
+    }
+    if (id === 'flow-icon'){
+      this.componentCommunication.whichLeftSideNav = 3;
+    }
+    if (id === 'add-icon'){
+      this.componentCommunication.whichLeftSideNav = 4;
+    }
+    this.graphProvider.ogma.getEdges().setAttributes({color: this.constVlues.standardEdgeColor, text: null, width: 1});
+    this.graphProvider.ogma.getNodes().setAttributes({color: this.constVlues.standardNodeColor, text: null});
+  }
+
+
 }
